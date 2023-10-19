16bc281b2791cdf96b7a9ccf2d06382ea3330274
yu.wang <wangyu@wycode.cn>
2023-10-19 18:55:36 +0800

    save goods detail before pay

--- src/controllers/vending.ts
+++ src/controllers/vending.ts
@@ -1,9 +1,10 @@
-import { helpers, WxPay } from "../../deps.ts";
+import { helpers, ObjectId, WxPay } from "../../deps.ts";
 import { COLLECTIONS, db } from "../mongo.ts";
 import { Context } from "../types.ts";
 import { getDataResult, getErrorResult } from "../utils.ts";
 import { logger } from "../logger.ts";
 import { env } from "../env.ts";
+import { sendEmail } from "../notifier.ts";
 
 let wxPay: WxPay;
 
@@ -23,24 +24,23 @@ export async function getGoods(ctx: Context) {
 export async function getOrder(ctx: Context) {
   const { id } = helpers.getQuery(ctx, { mergeParams: true });
   const cc = db.collection(COLLECTIONS.VENDING_ORDER);
-  const result = await cc.findOne({ out_trade_no: id });
+  const result = await cc.findOne({ _id: id });
   ctx.response.body = getDataResult(result);
 }
 
 export async function createOrder(ctx: Context) {
-  const { description, total, goods_detail } = await ctx.request.body().value;
-  logger.info("createOrder:", description, total, goods_detail.length);
-  if (!description || !total || !goods_detail || !goods_detail.length) {
+  const { description, total, goodsDetail } = await ctx.request.body().value;
+  logger.info("createOrder:", description, total, goodsDetail.length);
+  if (!description || !total || !goodsDetail || !goodsDetail.length) {
     ctx.throw(400);
   }
-  const out_trade_no = new Date().getTime().toString();
+  const out_trade_no = new ObjectId();
   const params = {
     description,
     appid: env.VENDING_APP_ID,
     mchid: env.VENDING_MCH_ID,
     out_trade_no,
     amount: { total },
-    detail: { goods_detail },
     notify_url: "https://wycode.cn/api/v1/vending/wx-notify",
   };
   const nonce_str = Math.random().toString(36).substring(2, 15), // 随机字符串
@@ -65,7 +65,18 @@ export async function createOrder(ctx: Context) {
   const data = await res.json();
   data.out_trade_no = out_trade_no;
   logger.info("createOrder: data: ", JSON.stringify(data));
-  ctx.response.body = getDataResult(data);
+  if (res.status === 200) {
+    const cc = db.collection(COLLECTIONS.VENDING_ORDER);
+    await cc.insertOne({
+      _id: out_trade_no,
+      createDate: new Date(),
+      trade_state: "CREATED",
+      goodsDetail,
+    });
+    ctx.response.body = getDataResult(data);
+  } else {
+    ctx.response.body = getErrorResult(data);
+  }
 }
 
 export async function notify(ctx: Context) {
@@ -92,20 +103,23 @@ export async function notify(ctx: Context) {
       body.resource.ciphertext,
       body.resource.associated_data,
       body.resource.nonce,
-    );
-    logger.info("解密结果:", JSON.stringify(result));
+      // deno-lint-ignore no-explicit-any
+    ) as any;
+    const resultString = JSON.stringify(result);
+    logger.info("解密结果:", resultString);
     if (!result) ctx.throw(403, "解密失败");
 
     const cc = db.collection(COLLECTIONS.VENDING_ORDER);
-    await cc.insertOne(result);
+    await cc.updateOne({ _id: new ObjectId(result.out_trade_no) }, result);
+    sendEmail(`订单支付成功：\n${resultString}`);
   }
 
-  ctx.response.body = "ok";
+  ctx.response.body = 'ok';
 }
 
 export function getWxPay() {
   if (!wxPay) {
-    const { cert, key } = JSON.parse(env.VENDING_WX_API_CLINET_CERT);
+    const { cert, key } = JSON.parse(env.VENDING_WX_API_CLIENT_CERT);
 
     wxPay = new WxPay({
       appid: env.VENDING_APP_ID,
